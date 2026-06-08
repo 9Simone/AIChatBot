ca27205a06142ef6cfa5aff7b772133a6029d5bd
Simone Mancinelli <simone.mancinelli@sourcesense.com>
2026-06-08 17:03:47 +0200

    first commit

---
+++ src/test/java/com/ai/chatbot/AIChatBot/AiChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.chatbot.AIChatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
